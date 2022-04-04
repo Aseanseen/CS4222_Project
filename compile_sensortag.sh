187c86a98f478fd1750ec48e2baf89c3371e4e3b
sean <sean.lim.110@gmail.com>
2022-04-04 15:59:09 +0800

    update gitignore

--- compile_sensortag.sh
+++ compile_sensortag.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-if [ "$#" -eq 0 ]; then
-    echo "No binary file provided"
-    exit 1
-elif [ "$#" -ne 2 ]; then
-    echo "You must enter 2 binary files"
-    exit 1
-fi
-
-make TARGET=srf06-cc26xx BOARD=sensortag/cc2650 $1 $2 CPU_FAMILY=cc26xx
+make TARGET=srf06-cc26xx BOARD=sensortag/cc2650 token_1.bin token_2.bin CPU_FAMILY=cc26xx
